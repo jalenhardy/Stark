afcc2ce37c3bc2bf93d570f3b9bcd1a21966a3fe
Erick <erickgnjoroge@yahoo.com>
2018-03-24 18:41:30 -0400

    Added a Token class

--- src/Token.java
+++ src/Token.java
@@ -1,5 +1,20 @@
-
-
 public class Token{
-
+    TokenType type;
+    String value;
+    Token(TokenType Type){//Token(Function)
+        this.type = Type;
+    }
+    Token(TokenType Type, String Value){//Token(Intenger, "2") or Token(Float, "1.2")
+        this.type = Type;
+        this.value = Value;
+    }
+    public TokenType getType(){
+        return this.type;
+    }
+    public String getValue(){
+        return this.value;
+    }
+    public Token getToken(){
+        return this;
+    }
 }
